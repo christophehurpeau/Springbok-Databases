b9699ece9bce63ecf8da4d689c0d9d282b8266e0
Christophe Hurpeau <christophe@hurpeau.com>
2012-12-09 12:01:50 +0100

    remove HHtml::jsCompat()

--- viewsLayouts/base.php
+++ viewsLayouts/base.php
@@ -3,7 +3,6 @@
 	<head>
 		<? HHtml::metaCharset() ?>
 		<title>{$layout_title}</title>
-		<!--[if lt IE 9]><?php HHtml::jsLink('/ie-lt9') ?><![endif]-->
 		<?php HHtml::cssLink(); HHtml::jsLink(); HHtml::jsI18n() ?>
 		<? HHtml::favicon() ?>
 	</head>
